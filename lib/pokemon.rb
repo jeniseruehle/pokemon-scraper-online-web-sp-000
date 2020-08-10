e91db53037aeb02f5a9d82f7d79bc25b00d4cd73
Jenise Ruehle <jela07@gmail.com>
2020-08-10 18:32:36 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,6 +8,7 @@ class Pokemon
     @db = db
   end 
   
+<<<<<<< HEAD
   def self.save(name, type, db) 
       sql = <<-SQL
       INSERT INTO pokemon (name, type) VALUES (?, ?);
@@ -23,5 +24,20 @@ class Pokemon
     
     result = db.execute(sql, id).flatten
     Pokemon.new(name: result[1], type: result[2], db: db, id: id)
+=======
+  def self.save 
+      sql = <<-SQL
+      INSERT INTO pokemon (name, type, db)
+      VALUES (?, ?, ?)
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.type, self.db)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end 
+  end 
+  
+  def find
+    
+>>>>>>> fca240eefbfaaa0d0feb030e8e0b1c65d390be43
   end
 end
